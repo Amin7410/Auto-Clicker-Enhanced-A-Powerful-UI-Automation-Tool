51d2449c1a155b1f9a8f33888ba6ce324d080c92
Amin <minhdinhkhoi@gmail.com>
2025-12-16 02:16:42 +0700

    Update parsing_utils.py

--- autoclicker/utils/parsing_utils.py
+++ autoclicker/utils/parsing_utils.py
@@ -23,19 +23,3 @@ def parse_tuple_str(tuple_str: str | None, expected_len: int, item_type=int) -> 
     except Exception as e:
         logger.error(f"Unexpected error parsing tuple string '{tuple_str}': {e}", exc_info=True)
         return None
-    
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
