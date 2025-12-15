8484c8239ca77808bb0d9ef51405c853b61c5b09
Amin <minhdinhkhoi@gmail.com>
2025-12-16 01:49:25 +0700

    Update action.py

--- autoclicker/core/action.py
+++ autoclicker/core/action.py
@@ -527,20 +527,5 @@ if not hasattr(Action, 'safe_int_or_none'):
          except (ValueError, TypeError):
              return None
     Action.safe_int_or_none = _action_safe_int_or_none_helper
-
-
-# Auto Clicker Enhanced
-# Copyright (C) <2025> <Đinh Khởi Minh>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
